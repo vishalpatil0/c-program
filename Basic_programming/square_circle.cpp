2af629c857d6bba1faa4127d8011303f36b65fe0
Vishal Patil <vishalgpatil10@gmail.com>
2020-02-08 22:40:53 +0530

    area of square and circumference of circle

---
+++ Basic_programming/square_circle.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int area,side;
+	float cir,r;
+	cout<<"Enter the side of square"<<endl;
+	cin>>side;
+	area=side*side;
+	cout<<"Area of square is = "<<area<<endl;
+	cout<<"Enter the radius of circle"<<endl;
+	cin>>r;
+	cir=2*3.14*r;
+	cout<<"Circumference of circle is = "<<cir<<endl;
+	return 100;
+}
