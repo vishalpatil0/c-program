c373a431a5fcb16b366a53f2fdb17db3e0f4d151
Vishal Patil <vishalgpatil10@gmail.com>
2020-02-15 12:44:46 +0530

    basic

---
+++ Basic_programming/float1.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void main()
+{
+	float x=8900.768;
+	
+	int a=12;
+	//printf("%06.2f\n",x);
+	//printf("%6f\n",x);
+	printf("%-10d,%d\n",a,5);
+	//printf("%.2f\n",x);
+//	printf("%10.2f\n",x);
+//	printf("%8d\n",a);
+//	printf("%08d\n",a);
+}
